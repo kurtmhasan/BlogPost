6e5f75759cb699a41dbaa957a472ae486b0f89cb
Hasan <kurtmhasan351@gmail.com>
2025-10-30 10:05:58 +0300

    like fonksiyonu yanlış çalışıyor diğer temel özellikler yorum post listeleme işlemleri düzgün çalışıyor

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Like;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LikeController extends Controller
+{
+    //
+    public function likePost(Request $request, $post_id){
+        $exists = Like::where('post_id', $post_id)->exists();
+
+        if(!$exists){
+            $post = Post::findOrFail($post_id);
+            $like = new Like();
+            $like->post()->associate($post);
+            $like->user()->associate($request->user());
+            $like->save();
+        }
+        else{
+            $like = Like::where('post_id', $post_id)->first();
+            $like->delete();
+        }
+        return redirect()->back();
+    }
+
+    public function countLike($post_id){
+        dd($post_id);
+        return view('front.posts.index', compact('sayi'));
+    }
+}
